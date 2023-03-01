1b7f96fedcc13470d4e22a43c5df0830cc6bf25b
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-03-01 20:10:14 +1300

    ...

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p style="color:green">Coding my website</p>
+<!-- CC - styling -->
+<style>
+
+    h1{
+        color:blue;
+        font-size:70px;
+        font-style: italic;
+        font-weight:normal;
+    }
+</style>
+<!-- JS - functionality -->>
+<script>
+
+    
+
+
+
+
+   let count = 0;
+   let first_name = "Ben"
+   let last_name = "Campbell"
+   const space = " "
+
+   function incrementCount()
+   {
+        //count = count+1
+        count++;
+   }
+
+  
+
+   function writeName()
+   {
+        return (first_name + space + last_name)
+   }
+
+   let randomColour;
+
+   function changeColour()
+   {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+
+        randomColour = 'rgb(${r} ,${g} ${b})';
+   }
+
+</script>
+
+
+
+<h1>{writeName()}</h1>
+<!-- HTML - structure -->
+<button on:click={changeColour}>
+    Clicked {count} {count == 1 ? 'time' : 'times'}
+</button>
+
+
+
+<div>
+    (writeName())
+</div>
